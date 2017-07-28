ff8f2e7e7ff223e7c9a687987b00dbf1708d63f8
Cristrian Traina <crissstian96@gmail.com>
2017-07-28 18:01:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,145 @@
+/*!
+ * static-angular
+ * MIT Licensed
+*/
+'use strict';
+
+/**
+ * Module dependencies
+ * @private
+ */
+
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * @param {string | object} [config]
+ * @return {function}
+ * @public
+ */
+module.exports = function serveAngular(config) {
+  let cfg;
+  let angularPath, angularAssetsPath, files, assets;
+
+  /**
+   * default config object
+   * @private
+   */
+  var defaultConfig = {
+    path: path.join('client','dist'),
+    assetsFolder: 'assets',
+    assetsUrl: '/assets'
+  };
+
+  //working directory
+  let wkd = path.normalize(path.dirname(module.parent.id));
+
+  //if the type of parameter is a string, assume it is the path
+  if (typeof config === 'string')
+    cfg = Object.assign(defaultConfig, { path: config });
+  else if (typeof config === 'object')
+    cfg = Object.assign(defaultConfig, config);
+  else if (config === undefined || config === null)
+    cfg = defaultConfig;
+  else 
+    throw new TypeError('Parameter type must be a string or an object, ' + typeof config + ' received');
+
+  if(!cfg.assetsUrl.startsWith('/')) throw new Error('assetsUrl parameter should start with /');
+  if(this) this.cfg = cfg;
+
+  angularPath = path.join(wkd, cfg.path);
+  angularAssetsPath = path.join(wkd, cfg.path, cfg.assetsFolder);
+
+  assets = fs.existsSync(angularAssetsPath) ? fs.readdirSync(angularAssetsPath) : [];
+  if(!fs.existsSync(angularPath)) throw new Error('Cannot find folder ' + angularPath);
+  files = fs.readdirSync(angularPath);
+
+  // middleware
+  return function serveAngular(req, res, next) {
+
+    if(req.method !== 'GET') return next();
+
+    if(!res.sendFile) {
+      res.sendFile = sendFile.bind(res); 
+    }
+
+    if (isOutOfContext(req.url)) {
+      let err = new Error('Cannot access page');
+      err.status = 400;
+      return next(err);
+    }
+
+    if (toBeEscaped(req.url, cfg.except)) {
+      return next();
+    }
+
+    //serve assets
+    
+
+    if(
+      req.url.endsWith('.jpg')  ||
+      req.url.endsWith('.jpeg') ||
+      req.url.endsWith('.gif')  ||
+      req.url.endsWith('.png')  ||
+      req.url.endsWith('.svg')
+    ) {
+      for(let i=0; i<assets.length; i++) {
+        if(path.dirname(req.url) === cfg.assetsUrl && assets[i] === path.basename(req.url)) 
+          return res.sendFile(path.join(angularAssetsPath, assets[i]));
+      }
+
+      return next(new Error('Image not found').status = 404);
+    }
+
+    for(let i=0; i<files.length; i++) {
+      if(files[i] === path.basename(req.url)) {
+        return res.sendFile(path.join(angularPath, files[i]));
+      }
+    }
+
+    res.sendFile.call(this, path.join(angularPath, 'index.html'));
+    }
+}
+
+/**
+ * Prevent users to get server files 
+ * @param {string} url 
+ * @param {object} res
+ * @private
+ */
+function isOutOfContext(url){
+  if (typeof url !== 'string') {
+    throw new TypeError('URL should be a string');
+  }
+
+  return url.indexOf('..')>-1;
+}
+
+/**
+ * Do not serve angular if the path is used for other purposes
+ * @private
+ */
+function toBeEscaped(url, escape) {
+  let escapeArray = [];
+  if (typeof escape === 'string')
+    escapeArray.push(escape);
+  if (typeof escape === 'object')
+    escapeArray = escape;
+  for(let i=0; i<escapeArray.length; i++){
+    let path = escapeArray[i];
+    if (path.match(/(\*\.)[a-z]{2,4}$/g)) {
+      if(path.substring(path.lastIndexOf('.')) === url.substring(url.lastIndexOf('.')))
+        return true;
+    }
+    else if (url === path || url.startsWith(path+'/')) {
+      return true;
+    }
+    // TODO add more use cases
+  }
+  return false;
+}
+
+function sendFile(path) {
+  fs.createReadStream(path)
+    .pipe(this);
+}
---
+++ test/test.js
@@ -0,0 +1,152 @@
+process.env.NODE_ENV = 'test';
+
+var should = require('should');
+var angular = require('..');
+var http = require('http');
+var path = require('path');
+const fs = require('fs');
+var request = require('supertest');
+var ctx;
+var server;
+describe('serve-angular', function () {
+
+  describe('#params', function () {
+
+    beforeEach(function () {
+      ctx = {};
+    });
+
+    it('should use default path if not received as parameter', function () {
+      angular.call(ctx, {a: 1, b: 2 });
+
+      should(ctx.cfg).hasOwnProperty('path');
+      should(ctx.cfg.path).be.equal(path.join('client', 'dist'));
+    });
+
+    it('should use default path when the parameter is void', function () {
+      angular.call(ctx);
+
+      should(ctx.cfg).hasOwnProperty('path');
+      should(ctx.cfg.path).be.equal(path.join('client', 'dist'));
+
+    });
+
+  });
+
+  describe('#serving root', function () {
+
+    it('should serve cfg.path folder content when HTTP gets /', function (done) {
+      var cfg = {
+        path: path.join('client', 'custom')
+      };
+
+      server = createServer(cfg);
+
+      request(server)
+        .get('/')
+        .expect(200, 'custom', done);
+    });
+
+    it('should serve /client/dist content when HTTP gets / and no cfg is provided', function (done) {
+      let server = createServer();
+
+      request(server)
+        .get('/')
+        .expect(200, 'test', done);
+    });
+
+    it('should serve cfg.path content when HTTP gets any url', function (done) {
+      let server = createServer();
+
+      request(server)
+        .get('/meaninglessUrlToBeHandledByClient')
+        .expect(200, 'test', done);
+    });
+    
+    it('should call next() if the url is contained in cfg.except', function (done) {
+      let cfg = {
+       except: '/api' 
+      };
+
+      let server = createServer(cfg);
+
+      request(server)
+        .get('/api')
+        .expect(404, 'Page not found', done);
+    });
+
+    it('should call next() if the url is contained in cfg.except array', function (done) {
+      let cfg = {
+        except: ['/a', '/b', '/c', '/d', '/e']
+      };
+
+      let server = createServer(cfg);
+
+      request(server)
+        .get('/d')
+        .expect(404, 'Page not found', done);
+    });
+
+    it('should not call next() if the url starts with a cfg.except element', function (done) {
+      let cfg = {
+        except: ['/api', '/user']
+      };
+
+      let server = createServer(cfg);
+
+      request(server)
+        .get('/users')
+        .expect(200, 'test', done);
+    });
+
+    it('should call next() if the url is a child of a cfg.except element', function (done) {
+      let cfg = {
+        except: ['/api', '/user']
+      };
+
+      let server = createServer(cfg);
+
+      request(server)
+        .get('/user/jack')
+        .expect(404, 'Page not found', done);
+    });
+
+    
+  });
+
+  describe('#serving assets', function () {
+
+    it('should serve cfg.assetsFolder inside cfg.path when the url ends with a image format', function (done) {
+      var cfg = {
+        assetsFolder: 'images',
+        assetsUrl: '/img'
+      };
+      
+      let server = createServer(cfg);
+      var img = fs.readFileSync(path.join('test', 'client', 'dist', 'images', 'stub-image.gif'));
+      request(server)
+        .get('/img/stub-image.gif')
+        .expect(200, '*probably a cat*', done);
+    });
+
+    it('should throw an error when the image doesn\'t exist', function (done) {
+      let server = createServer();
+
+      request(server)
+        .get('/assets/imageNotExist.png')
+        .expect(500, '', done);
+    });
+  });
+});
+
+function createServer(cfg) {
+  var _angular = require('..')(cfg);
+  var server = http.createServer(function(req, res) {
+    _angular(req, res, function onNext(err) {
+      res.statusCode = err ? (err.status || 500) : 404
+      res.end(err ? err.message : 'Page not found')
+    });
+  });
+
+  return server;
+}
